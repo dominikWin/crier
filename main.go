5e278aba459d9cc1191dae34dda47179dd047ab5
Dominik Winecki <dominikwinecki@gmail.com>
2019-03-06 22:38:34 -0500

    Add port from args

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func parseArgs() uint16 {
+
+	var port uint16 = 6782
+
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		return port
+	}
+
+	if len(args)%2 != 0 {
+		log.Fatalln("Bad number of arguments, should be <key> <value> pairs!")
+	}
+
+	switch args[0] {
+	case "-p":
+		fallthrough
+	case "--port":
+		i, err := strconv.Atoi(args[1])
+		if err != nil {
+			log.Fatalln("Failed to parse port number!")
+		}
+		if i < 0 || i > 0xffff {
+			log.Fatalln("Port number out of range!")
+		}
+		port = uint16(i)
+	default:
+		log.Fatalln("Unknown CLI argument given!")
+	}
+
+	return port
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := parseArgs()
+	pid := os.Getpid()
+
+	log.Println("!!! Crier is starting !!!")
+	log.Printf("Port: %d  PID: %d", port, pid)
 }
