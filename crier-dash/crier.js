a85a3748b564ef498843b36aa65f59ac1df40d23
Dominik Winecki <dominikwinecki@gmail.com>
2019-03-14 00:48:04 -0400

    Add multi host seperation

--- crier-dash/crier.js
+++ crier-dash/crier.js
@@ -1,5 +1,7 @@
 var message_container = document.getElementById("message_container");
-var contents = "";
+var old_contents = "";
+var active_host = "";
+var active_messages = [];
 
 var websocket = new WebSocket("ws://localhost:8000/ws");
 
@@ -7,27 +9,39 @@ websocket.onopen = function () {
     websocket.send('init');
 };
 
-function message_to_table(msg) {
-    return `<table class="ui celled very compact red table">
+function messages_to_table(msg, color) {
+    var rows = "";
+    for(var i = 0; i < msg.length; i++) {
+        rows = `<tr>
+        <td>${msg[i].id}</td>
+        <td>${msg[i].host}</td>
+        <td>${msg[i].message_head}</td>
+        </tr>` + rows;
+    }
+    return `<table class="ui celled very compact ${color} table">
     <thead>
         <tr>
-            <th>Time</th>
-            <th>Host</th>
-            <th>Message</th>
+            <th width="10%">ID</th>
+            <th width="12%">Host</th>
+            <th width="78%">Message</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-        <td>${msg.id}</td>
-        <td>${msg.host}</td>
-        <td>${msg.message_head}</td>
-        </tr>
+        ${rows}
     </tbody>
 </table>`;
 }
 
 function add_message(msg) {
-    contents = message_to_table(msg) + contents;
+    if(msg.host != active_host) {
+        if(active_messages.length > 0) {
+            old_contents = messages_to_table(active_messages) + old_contents;
+        }
+        active_messages = [];
+    }
+    active_host = msg.host;
+    active_messages = active_messages.concat([msg]);
+    var contents = messages_to_table(active_messages) + old_contents;
     message_container.innerHTML = contents;
 }
 
